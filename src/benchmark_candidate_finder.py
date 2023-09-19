bcc70c46d8c6e4713f9e192e4949dcb890faf492
Ali Altıparmak <ali@huridocs.org>
2023-09-19 15:03:24 +0300

    Update candidate benchmark

--- src/benchmark_candidate_finder.py
+++ src/benchmark_candidate_finder.py
@@ -2,6 +2,7 @@ import os
 from os.path import join
 from pathlib import Path
 from time import time
+from pdf_features.PdfPage import PdfPage
 from pdf_reading_order.PdfReadingOrderTokens import PdfReadingOrderTokens
 from sklearn.metrics import f1_score, accuracy_score
 from pdf_reading_order.config import ROOT_PATH, PDF_LABELED_DATA_ROOT_PATH
@@ -12,34 +13,45 @@ from pdf_reading_order.model_configuration import CANDIDATE_MODEL_CONFIGURATION
 BENCHMARK_MODEL_PATH = Path(join(ROOT_PATH, "model", "candidate_selector_benchmark.model"))
 
 
-def loop_pdf_reading_order_tokens(pdf_reading_order_tokens_list: list[PdfReadingOrderTokens]):
+def loop_pdf_reading_order_tokens(pdf_reading_order_tokens_list: list[PdfReadingOrderTokens], page: PdfPage):
     for pdf_reading_order_tokens in pdf_reading_order_tokens_list:
-        for page in pdf_reading_order_tokens.pdf_features.pages:
-            page.tokens = sorted(page.tokens, key=lambda token: pdf_reading_order_tokens.reading_order_by_token[token])
-            for i in range(len(page.tokens)):
-                yield i, len(page.tokens[i:])
+        if page not in pdf_reading_order_tokens.labeled_page_by_raw_page:
+            continue
+        yield pdf_reading_order_tokens
+
+
+def loop_current_token_candidate_token_labels(trainer, pdf_reading_order_tokens_list: list[PdfReadingOrderTokens]):
+    for current_token, possible_candidate_token, page in trainer.loop_token_combinations():
+        for pdf_reading_order_tokens in loop_pdf_reading_order_tokens(pdf_reading_order_tokens_list, page):
+            label = pdf_reading_order_tokens.labeled_page_by_raw_page[page].is_next_token(
+                current_token, possible_candidate_token
+            )
+            yield label
 
 
 def train_for_benchmark():
     pdf_reading_order_tokens_list = load_labeled_data(PDF_LABELED_DATA_ROOT_PATH, filter_in="train")
-    labels = []
-    for i, candidate_tokens_size in loop_pdf_reading_order_tokens(pdf_reading_order_tokens_list):
-        labels += [1] + [0] * (candidate_tokens_size-1)
 
     pdf_features_list = [pdf_reading_order_tokens.pdf_features for pdf_reading_order_tokens in pdf_reading_order_tokens_list]
     trainer = ReadingOrderCandidatesTrainer(pdf_features_list, CANDIDATE_MODEL_CONFIGURATION)
+
+    labels = []
+    for label in loop_current_token_candidate_token_labels(trainer, pdf_reading_order_tokens_list):
+        labels.append(label)
+
     os.makedirs(BENCHMARK_MODEL_PATH.parent, exist_ok=True)
     trainer.train(str(BENCHMARK_MODEL_PATH), labels)
 
 
 def predict_for_benchmark():
     pdf_reading_order_tokens_list = load_labeled_data(PDF_LABELED_DATA_ROOT_PATH, filter_in="test")
-    truths = []
-    for i, candidate_tokens_size in loop_pdf_reading_order_tokens(pdf_reading_order_tokens_list):
-        truths += [1] + [0] * (candidate_tokens_size-1)
 
     pdf_features_list = [pdf_reading_order_tokens.pdf_features for pdf_reading_order_tokens in pdf_reading_order_tokens_list]
     trainer = ReadingOrderCandidatesTrainer(pdf_features_list, CANDIDATE_MODEL_CONFIGURATION)
+
+    truths = []
+    for label in loop_current_token_candidate_token_labels(trainer, pdf_reading_order_tokens_list):
+        truths.append(label)
     print("predicting")
     predictions = trainer.predict(BENCHMARK_MODEL_PATH)
     print(len(truths))
