2213664a6dc9e8686e1291c9aafeb4970a764344
Ali Altıparmak <ali@huridocs.org>
2023-09-19 15:02:49 +0300

    Update candidates trainer

--- src/pdf_reading_order/ReadingOrderCandidatesTrainer.py
+++ src/pdf_reading_order/ReadingOrderCandidatesTrainer.py
@@ -1,39 +1,39 @@
 from pathlib import Path
 import numpy as np
-from pdf_features.PdfPage import PdfPage
+from itertools import permutations
 from pdf_features.PdfToken import PdfToken
 from pdf_tokens_type_trainer.PdfTrainer import PdfTrainer
 
 
 class ReadingOrderCandidatesTrainer(PdfTrainer):
-
     @staticmethod
     def get_candidate_token_features(token_1: PdfToken, token_2: PdfToken):
-        return [token_1.bounding_box.top, token_1.bounding_box.left, token_1.bounding_box.right, token_1.bounding_box.bottom,
-                token_2.bounding_box.top, token_2.bounding_box.left, token_2.bounding_box.right, token_2.bounding_box.bottom,
-                token_1.bounding_box.bottom - token_2.bounding_box.top
-                ]
+        return [
+            token_1.bounding_box.top,
+            token_1.bounding_box.left,
+            token_1.bounding_box.right,
+            token_1.bounding_box.bottom,
+            token_2.bounding_box.top,
+            token_2.bounding_box.left,
+            token_2.bounding_box.right,
+            token_2.bounding_box.bottom,
+            token_1.bounding_box.bottom - token_2.bounding_box.top,
+        ]
 
     def loop_pages(self):
         for pdf_features in self.pdfs_features:
             for page in pdf_features.pages:
                 yield page
 
-    def loop_token_combinations_in_page(self, page: PdfPage):
-        sorted_page_tokens = [self.get_padding_token(-1, page.page_number)] + page.tokens
-        for i, current_token in enumerate(sorted_page_tokens):
-            possible_candidate_tokens = sorted_page_tokens[i + 1:]
-            for possible_candidate_token in possible_candidate_tokens:
-                yield current_token, possible_candidate_token
-
     def loop_token_combinations(self):
         for page in self.loop_pages():
-            for current_token, possible_candidate_token in self.loop_token_combinations_in_page(page):
-                yield current_token, possible_candidate_token
+            page_tokens = [self.get_padding_token(-1, page.page_number)] + page.tokens
+            for current_token, possible_candidate_token in permutations(page_tokens, 2):
+                yield current_token, possible_candidate_token, page
 
     def get_model_input(self):
         features_rows = []
-        for current_token, possible_candidate_token in self.loop_token_combinations():
+        for current_token, possible_candidate_token, _ in self.loop_token_combinations():
             features_rows.append(self.get_candidate_token_features(current_token, possible_candidate_token))
 
         return self.features_rows_to_x(features_rows)
