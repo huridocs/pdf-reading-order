0086106db245217334197838b474139c830517b6
Ali Altıparmak <ali@huridocs.org>
2023-12-08 18:16:26 +0300

    Add table figure preprocessing

--- src/predict.py
+++ src/predict.py
@@ -1,14 +1,21 @@
 import typer
+from pdf_tokens_type_trainer.TokenTypeTrainer import TokenTypeTrainer
 from OrderedPage import OrderedPage
 from pdf_features.PdfFeatures import PdfFeatures
+from TableFigureProcessor import TableFigureProcessor
 from pdf_reading_order.PdfReadingOrderTokens import PdfReadingOrderTokens
 from pdf_tokens_type_trainer.ModelConfiguration import ModelConfiguration
 from pdf_reading_order.ReadingOrderTrainer import ReadingOrderTrainer
 
 
-def predict(pdf_path: str, model_path: str = None):
+def predict(pdf_path: str, extract_figures_and_tables: bool = False, model_path: str = None):
     pdf_features = PdfFeatures.from_pdf_path(pdf_path)
     pdf_reading_order_tokens = PdfReadingOrderTokens(pdf_features, {})
+    if extract_figures_and_tables:
+        token_type_trainer = TokenTypeTrainer([pdf_features])
+        token_type_trainer.set_token_types()
+        table_figure_processor = TableFigureProcessor([pdf_reading_order_tokens])
+        table_figure_processor.process()
     trainer = ReadingOrderTrainer([pdf_reading_order_tokens], ModelConfiguration())
     trainer.predict(model_path)
 
