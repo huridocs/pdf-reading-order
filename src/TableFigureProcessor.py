7fc76244bc23a2af9406d473d9c61523ca0819ca
Ali Altıparmak <ali@huridocs.org>
2023-12-08 18:15:36 +0300

    Get processing when there are no labels

--- src/TableFigureProcessor.py
+++ src/TableFigureProcessor.py
@@ -26,8 +26,10 @@ class TableFigureProcessor:
         token_id = f"p{page_number}_m{token_id_number}"
         content = " ".join([token.content for token in paragraph_tokens])
         pdf_font = mode([token.font for token in paragraph_tokens])
-        reading_order = min([label_page.reading_order_by_token_id[token.id] for token in paragraph_tokens])
-        label_page.reading_order_by_token_id[token_id] = reading_order
+        reading_order = -1
+        if label_page:
+            reading_order = min([label_page.reading_order_by_token_id[token.id] for token in paragraph_tokens])
+            label_page.reading_order_by_token_id[token_id] = reading_order
         bounding_box = Rectangle.merge_rectangles([token.bounding_box for token in paragraph_tokens])
         token_type = mode([token.token_type for token in paragraph_tokens])
         return PdfToken(page_number, token_id, content, pdf_font, reading_order, bounding_box, token_type)
@@ -59,12 +61,17 @@ class TableFigureProcessor:
         if segment_type not in {TokenType.FIGURE, TokenType.TABLE}:
             return
         page = pdf_reading_order_tokens.pdf_features.pages[paragraph.tokens[0].page_number - 1]
-        label_page = pdf_reading_order_tokens.labeled_page_by_raw_page[page]
+        label_page = (
+            pdf_reading_order_tokens.labeled_page_by_raw_page[page]
+            if page in pdf_reading_order_tokens.labeled_page_by_raw_page
+            else None
+        )
         figure_table_token = self.get_processed_token_from_paragraph(paragraph.tokens, label_page)
         self.add_processed_token_to_page(figure_table_token, paragraph.tokens, page)
-        self.remove_paragraph_tokens_from_labels(paragraph.tokens, label_page)
         self.remove_paragraph_tokens_from_page(paragraph.tokens, page)
-        self.reassign_labels(label_page)
+        if label_page:
+            self.remove_paragraph_tokens_from_labels(paragraph.tokens, label_page)
+            self.reassign_labels(label_page)
 
     def process(self):
         paragraphs: list[Paragraph] = self.paragraph_extractor.get_paragraphs(self.model_path)
