2daa9cbe20dbb0d56c19560f1084f7a363ec1cf2
Gabo <gabriel.piles.glez@gmail.com>
2024-01-12 13:06:39 +0100

    Remove requirements from setup

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 PROJECT_NAME = "pdf-reading-order"
 
 setup(
     name=PROJECT_NAME,
     packages=["pdf_reading_order"],
     package_dir={"": "src"},
-    version="0.22",
+    version="0.23",
     url="https://github.com/huridocs/pdf-reading-order",
     author="HURIDOCS",
-    description="This tool returns the reading order of a PDF",
-    install_requires=requirements,
+    description="This tool returns the reading order of a PDF"
 )
